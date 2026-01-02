238989a24507e7b6ffc720af42c6d5d39f61d64a
gauravmalhotra3300-hub <gaurav.malhotra3300@gmail.com>
2026-01-02 20:36:27 +0530

    Add __init__.py - Python package initialization

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Windows Registry Monitor - Package initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Cybersecurity Team"
+__description__ = "Windows Registry Change Monitoring System"
